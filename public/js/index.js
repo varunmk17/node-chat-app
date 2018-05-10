ddefa760a3e203b103401a3a56b57e7b158db43c
varunmk17 <varunmk17@gmail.com>
2018-05-09 22:32:48 -0400

    Emit newmessage on createmessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('connected to server');
-
-    socket.emit('createMessage', {
-        from: 'varunk@gmail.com',
-        text: 'hey..'
-    });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected..');
 
-    socket.emit('newMessage', {
-        from: 'varun',
-        text: 'Yepp...',
-        createdAt: 1233232
-    });
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
 
-    socket.on('createMessage', (createMessage) => {
-        console.log('createMessage', createMessage);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
