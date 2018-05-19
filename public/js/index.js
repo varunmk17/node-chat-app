2bb64ec1f1bb5376dc9962e183f6905357e7d44b
varunmk17 <varunmk17@gmail.com>
2018-05-19 19:30:20 -0400

    Add mustache.js

--- public/js/index.js
+++ public/js/index.js
@@ -10,28 +10,30 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     var formattedtime = moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${formattedtime}: ${message.text}`);
-
-    jQuery('#messages').append(li);
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedtime
+    });
+    jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function (message) {
     var formattedtime = moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My Current Location</a>')
-    li.text(`${message.from}: ${formattedtime}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    
-    jQuery('#messages').append(li);
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template, {
+        url: message.url,
+        from: message.from,
+        createdAt: formattedtime
+    });
+    jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault();
-
+    
     var messageTextBox = jQuery('[name=message]');
-
     socket.emit('createMessage', {
         from: 'User',
         text: messageTextBox.val()
@@ -46,11 +48,8 @@ locationButton.on('click', function() {
     if (!navigator.geolocation) {
         return alert('Geolocaction not supported');
     }
-
     locationButton.attr('disabled', 'disabled').text('Sending location..');
-
     navigator.geolocation.getCurrentPosition(function (position) {
-
         locationButton.removeAttr('disabled').text('Sending location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
